49c969f2bbacc508b58c664cce91f66ce302dcf9
Jorge <jogonzal@microsoft.com>
2016-05-13 17:21:52 -0700

    Then chaining

--- Pluralsight/ES6PromiseV1.js
+++ Pluralsight/ES6PromiseV1.js
@@ -2,11 +2,20 @@ function doAsync() {
     let p = new Promise (function(resolve, reject) {
         console.log("in promise code");
         setTimeout(function() {
-            console.log("resolving... ");
-            resolve();
+            console.log("rejecting... ");
+            reject("rejectedValue");
         }, 2000);
     });
     return p;
 }
 
-let promise = doAsync();
+let promise = doAsync().then(function(val){
+    console.log("Fulfilled! " + val);
+    return "fulfilledReturnValue";
+},
+function(val){
+    console.log("Rejected!" + val);
+    return "rejectedReturnValue";
+}).then(function(val){
+    console.log("something else... " + val);
+});
